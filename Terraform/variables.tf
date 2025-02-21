79e35996eac2a30695a729cf61e07167d8775850
GuillaumeGANGLOFF <148479543+GuillaumeGANGLOFF@users.noreply.github.com>
2025-02-21 15:30:04 +0100

    Update from Epitech

--- Terraform/variables.tf
+++ Terraform/variables.tf
@@ -1,20 +1,4 @@
 # Variables
 variable "resource_group_name" {
-  default = "rg-groupe-XX"
-}
-
-variable "location" {
-  default = "westeurope"
-}
-
-variable "vm_count_vnet-internal-001" {
-  default = 1
-}
-
-variable "vm_count_vnet-connectivity-001" {
-  default = 1
-}
-
-variable "vm_count_vnet-internal-002" {
-  default = 2
+  default = "rg-group-16"
 }

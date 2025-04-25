2ba27626a95bb2227b50b2720c588c022bbafae2
GuillaumeGANGLOFF <148479543+GuillaumeGANGLOFF@users.noreply.github.com>
2025-04-25 07:15:53 +0200

    switch firewall vm os from ubuntu to freebsd

--- terraform/main.tf
+++ terraform/main.tf
@@ -515,9 +515,9 @@ resource "azurerm_linux_virtual_machine" "vm-firewall-001" {
     storage_account_type = "Standard_LRS"
   }
   source_image_reference {
-    publisher = "Canonical"
-    offer     = "0001-com-ubuntu-server-jammy"
-    sku       = "22_04-lts"
+    publisher = "thefreebsdfoundation"
+    offer     = "freebsd-14_2"
+    sku       = "14_2-release-amd64-gen2-ufs"
     version   = "latest"
   }
 }

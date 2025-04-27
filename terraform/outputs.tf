eae313fd2c8a2da669da3510b48d11eed780fc8e
GuillaumeGANGLOFF <148479543+GuillaumeGANGLOFF@users.noreply.github.com>
2025-04-27 19:10:35 +0200

    ajout des sorties pour obtenir les ips privées et publiques

---
+++ terraform/outputs.tf
@@ -0,0 +1,27 @@
+output "connectivity_vm_public_ip" {
+  value = azurerm_public_ip.vnet-connectivity-001.ip_address
+}
+
+output "vm-web-private_ip" {
+  value = azurerm.network_interface.nic-site-web-001.private_ip_address
+}
+
+output "vm-ftp-private_ip" {
+  value = azurerm.network_interface.nic-ftp-001.private_ip_address
+}
+
+output "vm-surveillance-private_ip" {
+  value = azurerm.network_interface.nic-surveillance-001.private_ip_address
+}
+
+output "vm-bastion-private_ip" {
+  value = azurerm.network_interface.nic-bastion-001.private_ip_address
+}
+
+output "vm-firewall-private_ip" {
+  value = azurerm.network_interface.nic-firewall-001.private_ip_address
+}
+
+output "vm-backup-private_ip" {
+  value = azurerm.network_interface.nic-backup-001.private_ip_address
+}

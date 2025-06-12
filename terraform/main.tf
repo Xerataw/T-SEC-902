80eb53fbae7ee06f2f5f254d799d07664620bb2f
GuillaumeGANGLOFF <148479543+GuillaumeGANGLOFF@users.noreply.github.com>
2025-06-12 15:19:30 +0200

    update

--- terraform/main.tf
+++ terraform/main.tf
@@ -11,24 +11,6 @@ data "azurerm_resource_group" "rg" {
 }
 
 # Règle de sécurités réseaux 
-resource "azurerm_network_security_group" "nic-backup-001-nsg" {
-  name                = "nic-backup-001-nsg"
-  location            = data.azurerm_resource_group.rg.location
-  resource_group_name = data.azurerm_resource_group.rg.name
-
-  security_rule {
-    name                       = "Allow-SSH-From-Teleport"
-    priority                   = 4096
-    direction                  = "Inbound"
-    access                     = "Allow"
-    protocol                   = "Tcp"
-    source_port_range          = "*"
-    destination_port_range     = "3022"
-    source_address_prefixes    = ["VirtualNetwork"]
-    destination_address_prefix = "*"
-  }
-}
-
 resource "azurerm_network_security_group" "nic-ftp-001-nsg" {
   name                = "nic-ftp-001-nsg"
   location            = data.azurerm_resource_group.rg.location
@@ -42,25 +24,55 @@ resource "azurerm_network_security_group" "nic-ftp-001-nsg" {
     protocol                   = "Tcp"
     source_port_range          = "*"
     destination_port_range     = "3022"
-    source_address_prefixes    = ["VirtualNetwork"]
+    source_address_prefix      = "10.0.0.0/8"
     destination_address_prefix = "*"
   }
-}
-
-resource "azurerm_network_security_group" "nic-samba-001-nsg" {
-  name                = "nic-samba-001-nsg"
-  location            = data.azurerm_resource_group.rg.location
-  resource_group_name = data.azurerm_resource_group.rg.name
 
   security_rule {
-    name                       = "Allow-SSH-From-Teleport"
-    priority                   = 4096
+    name                       = "Allow-SSH"
+    priority                   = 100
     direction                  = "Inbound"
     access                     = "Allow"
     protocol                   = "Tcp"
     source_port_range          = "*"
-    destination_port_range     = "3022"
-    source_address_prefixes    = ["VirtualNetwork"]
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  security_rule {
+    name                       = "Allow-Rsyslog"
+    priority                   = 200
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Udp"
+    source_port_range          = "*"
+    destination_port_range     = "514"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  security_rule {
+    name                       = "Allow-FTP-From-Connectivity"
+    priority                   = 300
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "21"
+    source_address_prefix      = "10.1.0.0/16"
+    destination_address_prefix = "*"
+  }
+
+  security_rule {
+    name                       = "Allow-FTP-Passive-From-Connectivity"
+    priority                   = 400
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "1024-1048"
+    source_address_prefix      = "10.1.0.0/16"
     destination_address_prefix = "*"
   }
 }
@@ -78,7 +90,31 @@ resource "azurerm_network_security_group" "nic-web-001-nsg" {
     protocol                   = "Tcp"
     source_port_range          = "*"
     destination_port_range     = "3022"
-    source_address_prefixes    = ["VirtualNetwork"]
+    source_address_prefix      = "10.0.0.0/8"
+    destination_address_prefix = "*"
+  }
+
+  security_rule {
+    name                       = "Allow-SSH"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  security_rule {
+    name                       = "Allow-Rsyslog"
+    priority                   = 200
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Udp"
+    source_port_range          = "*"
+    destination_port_range     = "514"
+    source_address_prefix      = "*"
     destination_address_prefix = "*"
   }
 }
@@ -96,25 +132,31 @@ resource "azurerm_network_security_group" "nic-surveillance-001-nsg" {
     protocol                   = "Tcp"
     source_port_range          = "*"
     destination_port_range     = "3022"
-    source_address_prefixes    = ["VirtualNetwork"]
+    source_address_prefix      = "10.0.0.0/8"
     destination_address_prefix = "*"
   }
-}
-
-resource "azurerm_network_security_group" "nic-firewall-001-nsg" {
-  name                = "nic-firewall-001-nsg"
-  location            = data.azurerm_resource_group.rg.location
-  resource_group_name = data.azurerm_resource_group.rg.name
 
   security_rule {
-    name                       = "Allow-Https-For-Teleport"
-    priority                   = 3096
+    name                       = "Allow-SSH"
+    priority                   = 100
     direction                  = "Inbound"
     access                     = "Allow"
     protocol                   = "Tcp"
     source_port_range          = "*"
-    destination_port_range     = "443"
-    source_address_prefixes    = ["Internet"]
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  security_rule {
+    name                       = "Allow-Rsyslog"
+    priority                   = 200
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Udp"
+    source_port_range          = "*"
+    destination_port_range     = "514"
+    source_address_prefix      = "*"
     destination_address_prefix = "*"
   }
 }
@@ -188,36 +230,6 @@ resource "azurerm_subnet" "snet-internal-003" {
   address_prefixes     = ["10.3.1.0/24"]
 }
 
-# VNet 4
-resource "azurerm_virtual_network" "vnet-internal-004" {
-  name                = "vnet-internal-004"
-  location            = data.azurerm_resource_group.rg.location
-  resource_group_name = data.azurerm_resource_group.rg.name
-  address_space       = ["10.4.0.0/16"]
-}
-
-resource "azurerm_subnet" "snet-internal-004" {
-  name                 = "snet-internal-004"
-  resource_group_name  = data.azurerm_resource_group.rg.name
-  virtual_network_name = azurerm_virtual_network.vnet-internal-004.name
-  address_prefixes     = ["10.4.1.0/24"]
-}
-
-# VNet 5
-resource "azurerm_virtual_network" "vnet-internal-005" {
-  name                = "vnet-internal-005"
-  location            = data.azurerm_resource_group.rg.location
-  resource_group_name = data.azurerm_resource_group.rg.name
-  address_space       = ["10.5.0.0/16"]
-}
-
-resource "azurerm_subnet" "snet-internal-005" {
-  name                 = "snet-internal-005"
-  resource_group_name  = data.azurerm_resource_group.rg.name
-  virtual_network_name = azurerm_virtual_network.vnet-internal-005.name
-  address_prefixes     = ["10.5.1.0/24"]
-}
-
 # Peering entre les VNets
 resource "azurerm_virtual_network_peering" "vnet-internal-001_to_vnet-connectivity-001" {
   name                         = "vnet-internal-001-to-vnet-connectivity-001"
@@ -239,22 +251,6 @@ resource "azurerm_virtual_network_peering" "vnet-internal-003_to_vnet-connectivi
   name                         = "vnet-internal-003-to-vnet-connectivity-001"
   resource_group_name          = data.azurerm_resource_group.rg.name
   virtual_network_name         = azurerm_virtual_network.vnet-internal-003.name
-  remote_virtual_network_id    = azurerm_virtual_network.vnet-connectivity-001.id
-  allow_virtual_network_access = true
-}
-
-resource "azurerm_virtual_network_peering" "vnet-internal-004_to_vnet-connectivity-001" {
-  name                         = "vnet-internal-004-to-vnet-connectivity-001"
-  resource_group_name          = data.azurerm_resource_group.rg.name
-  virtual_network_name         = azurerm_virtual_network.vnet-internal-004.name
-  remote_virtual_network_id    = azurerm_virtual_network.vnet-connectivity-001.id
-  allow_virtual_network_access = true
-}
-
-resource "azurerm_virtual_network_peering" "vnet-internal-005_to_vnet-connectivity-001" {
-  name                         = "vnet-internal-005-to-vnet-connectivity-001"
-  resource_group_name          = data.azurerm_resource_group.rg.name
-  virtual_network_name         = azurerm_virtual_network.vnet-internal-005.name
   remote_virtual_network_id    = azurerm_virtual_network.vnet-connectivity-001.id
   allow_virtual_network_access = true
 }
@@ -283,22 +279,6 @@ resource "azurerm_virtual_network_peering" "vnet-connectivity-001_to_vnet-intern
   allow_virtual_network_access = true
 }
 
-resource "azurerm_virtual_network_peering" "vnet-connectivity-001_to_vnet-internal-004" {
-  name                         = "vnet-connectivity-001-to-vnet-internal-004"
-  resource_group_name          = data.azurerm_resource_group.rg.name
-  virtual_network_name         = azurerm_virtual_network.vnet-connectivity-001.name
-  remote_virtual_network_id    = azurerm_virtual_network.vnet-internal-004.id
-  allow_virtual_network_access = true
-}
-
-resource "azurerm_virtual_network_peering" "vnet-connectivity-001_to_vnet-internal-005" {
-  name                         = "vnet-connectivity-001-to-vnet-internal-005"
-  resource_group_name          = data.azurerm_resource_group.rg.name
-  virtual_network_name         = azurerm_virtual_network.vnet-connectivity-001.name
-  remote_virtual_network_id    = azurerm_virtual_network.vnet-internal-005.id
-  allow_virtual_network_access = true
-}
-
 # Table de routage
 resource "azurerm_route_table" "rt-networking-001" {
   name                = "rt-networking-001"
@@ -321,18 +301,6 @@ resource "azurerm_subnet_route_table_association" "rta-internal-snet-internal-00
 # Associer la table de routage aux sous-réseaux
 resource "azurerm_subnet_route_table_association" "rta-internal-snet-internal-003" {
   subnet_id      = azurerm_subnet.snet-internal-003.id
-  route_table_id = azurerm_route_table.rt-networking-001.id
-}
-
-# Associer la table de routage aux sous-réseaux
-resource "azurerm_subnet_route_table_association" "rta-internal-snet-internal-004" {
-  subnet_id      = azurerm_subnet.snet-internal-004.id
-  route_table_id = azurerm_route_table.rt-networking-001.id
-}
-
-# Associer la table de routage aux sous-réseaux
-resource "azurerm_subnet_route_table_association" "rta-internal-snet-internal-005" {
-  subnet_id      = azurerm_subnet.snet-internal-005.id
   route_table_id = azurerm_route_table.rt-networking-001.id
 }
 
@@ -450,7 +418,7 @@ resource "azurerm_linux_virtual_machine" "vm-ftp-001" {
   }
 }
 
-# Network Interface for each VM survaillance-001
+# Network Interface for each VM surveillance-001
 resource "azurerm_network_interface" "nic-surveillance-001" {
   name                = "nic-surveillance-001"
   location            = data.azurerm_resource_group.rg.location
@@ -483,96 +451,6 @@ resource "azurerm_linux_virtual_machine" "vm-surveillance-001" {
   }
 
   network_interface_ids = [azurerm_network_interface.nic-surveillance-001.id]
-  os_disk {
-    caching              = "ReadWrite"
-    storage_account_type = "Standard_LRS"
-  }
-  source_image_reference {
-    publisher = "Canonical"
-    offer     = "0001-com-ubuntu-server-jammy"
-    sku       = "22_04-lts"
-    version   = "latest"
-  }
-}
-
-# Network Interface for each VM samba-001
-resource "azurerm_network_interface" "nic-samba-001" {
-  name                = "nic-samba-001"
-  location            = data.azurerm_resource_group.rg.location
-  resource_group_name = data.azurerm_resource_group.rg.name
-  ip_configuration {
-    name                          = "ipconfig"
-    subnet_id                     = azurerm_subnet.snet-internal-004.id
-    private_ip_address_allocation = "Dynamic"
-  }
-}
-
-resource "azurerm_network_interface_security_group_association" "nic_samba_assoc" {
-  network_interface_id      = azurerm_network_interface.nic-samba-001.id
-  network_security_group_id = azurerm_network_security_group.nic-samba-001-nsg.id
-}
-
-# Virtual Machines
-resource "azurerm_linux_virtual_machine" "vm-samba-001" {
-  name                = "vm-samba-001"
-  location            = data.azurerm_resource_group.rg.location
-  resource_group_name = data.azurerm_resource_group.rg.name
-  ## 8$ par mois
-  size                            = "Standard_B1s"
-  admin_username                  = "adminuser"
-  disable_password_authentication = true
-
-  admin_ssh_key {
-    username   = "adminuser"
-    public_key = file("./terraform_azure_key_ssh/sec_azure_key.pub")
-  }
-
-  network_interface_ids = [azurerm_network_interface.nic-samba-001.id]
-  os_disk {
-    caching              = "ReadWrite"
-    storage_account_type = "Standard_LRS"
-  }
-  source_image_reference {
-    publisher = "Canonical"
-    offer     = "0001-com-ubuntu-server-jammy"
-    sku       = "22_04-lts"
-    version   = "latest"
-  }
-}
-
-# Network Interface for each VM backup-001
-resource "azurerm_network_interface" "nic-backup-001" {
-  name                = "nic-backup-001"
-  location            = data.azurerm_resource_group.rg.location
-  resource_group_name = data.azurerm_resource_group.rg.name
-  ip_configuration {
-    name                          = "ipconfig"
-    subnet_id                     = azurerm_subnet.snet-internal-005.id
-    private_ip_address_allocation = "Dynamic"
-  }
-}
-
-resource "azurerm_network_interface_security_group_association" "nic_backup_assoc" {
-  network_interface_id      = azurerm_network_interface.nic-backup-001.id
-  network_security_group_id = azurerm_network_security_group.nic-backup-001-nsg.id
-}
-
-# Virtual Machines
-resource "azurerm_linux_virtual_machine" "vm-backup-001" {
-  name                = "vm-backup-001"
-  location            = data.azurerm_resource_group.rg.location
-  resource_group_name = data.azurerm_resource_group.rg.name
-  ## 8$ par mois
-  size                            = "Standard_B1s"
-  admin_username                  = "adminuser"
-  disable_password_authentication = true
-
-  admin_ssh_key {
-    username   = "adminuser"
-    public_key = file("./terraform_azure_key_ssh/sec_azure_key.pub")
-  }
-
-  network_interface_ids = [azurerm_network_interface.nic-backup-001.id]
   os_disk {
     caching              = "ReadWrite"
     storage_account_type = "Standard_LRS"
@@ -626,51 +504,6 @@ resource "azurerm_linux_virtual_machine" "vm-bastion-001" {
     publisher = "Canonical"
     offer     = "0001-com-ubuntu-server-jammy"
     sku       = "22_04-lts"
-    version   = "latest"
-  }
-}
-
-# Network Interface for each VM firewall-001
-resource "azurerm_network_interface" "nic-firewall-001" {
-  name                = "nic-firewall-001"
-  location            = data.azurerm_resource_group.rg.location
-  resource_group_name = data.azurerm_resource_group.rg.name
-  ip_configuration {
-    name                          = "ipconfig"
-    subnet_id                     = azurerm_subnet.snet-connectivity-001.id
-    private_ip_address_allocation = "Dynamic"
-  }
-}
-
-resource "azurerm_network_interface_security_group_association" "nic_firewall_assoc" {
-  network_interface_id      = azurerm_network_interface.nic-firewall-001.id
-  network_security_group_id = azurerm_network_security_group.nic-firewall-001-nsg.id
-}
-
-# Virtual Machines
-resource "azurerm_linux_virtual_machine" "vm-firewall-001" {
-  name                = "vm-firewall-001"
-  location            = data.azurerm_resource_group.rg.location
-  resource_group_name = data.azurerm_resource_group.rg.name
-  ## 8$ par mois
-  size                            = "Standard_B1s"
-  admin_username                  = "adminuser"
-  disable_password_authentication = true
-
-  admin_ssh_key {
-    username   = "adminuser"
-    public_key = file("./terraform_azure_key_ssh/sec_azure_key.pub")
-  }
-
-  network_interface_ids = [azurerm_network_interface.nic-firewall-001.id]
-  os_disk {
-    caching              = "ReadWrite"
-    storage_account_type = "Standard_LRS"
-  }
-  source_image_reference {
-    publisher = "Debian"
-    offer     = "debian-12"
-    sku       = "12-gen2"
     version   = "latest"
   }
 }
--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,27 +1,24 @@
 output "connectivity_vm_public_ip" {
   value = azurerm_public_ip.connectivity_ip.ip_address
+  description = "Adresse IP publique de la VM de connectivité"
 }
 
-output "vm-web-private_ip" {
-  value = azurerm_network_interface.nic-site-web-001.private_ip_address
+output "vm_web_private_ip" {
+  value = azurerm_linux_virtual_machine.vm-site-web-001.private_ip_address
+  description = "Adresse IP privée de la VM Web"
 }
 
-output "vm-ftp-private_ip" {
-  value = azurerm_network_interface.nic-ftp-001.private_ip_address
+output "vm_ftp_private_ip" {
+  value = azurerm_linux_virtual_machine.vm-ftp-001.private_ip_address
+  description = "Adresse IP privée de la VM FTP"
 }
 
-output "vm-surveillance-private_ip" {
-  value = azurerm_network_interface.nic-surveillance-001.private_ip_address
+output "vm_surveillance_private_ip" {
+  value = azurerm_linux_virtual_machine.vm-surveillance-001.private_ip_address
+  description = "Adresse IP privée de la VM Surveillance"
 }
 
-output "vm-bastion-private_ip" {
-  value = azurerm_network_interface.nic-bastion-001.private_ip_address
-}
-
-output "vm-firewall-private_ip" {
-  value = azurerm_network_interface.nic-firewall-001.private_ip_address
-}
-
-output "vm-backup-private_ip" {
-  value = azurerm_network_interface.nic-backup-001.private_ip_address
+output "vm_bastion_private_ip" {
+  value = azurerm_linux_virtual_machine.vm-bastion-001.private_ip_address
+  description = "Adresse IP privée de la VM Bastion"
 }
